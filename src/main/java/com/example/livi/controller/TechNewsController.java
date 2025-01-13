c5ad04be1dd55fa103cdb0432552da63b78bc099
nhat <minhnhatt263@gmail.com>
2025-01-13 09:05:53 +0700

    update Api

--- src/main/java/com/example/livi/controller/TechNewsController.java
+++ src/main/java/com/example/livi/controller/TechNewsController.java
@@ -1,11 +1,15 @@
 package com.example.livi.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.example.livi.model.AboutLiviLife;
 import com.example.livi.model.TechNews;
 import com.example.livi.service.TechNewsService;
 
@@ -37,15 +41,43 @@ public class TechNewsController {
 	}
 	
 	@PostMapping("/{sessionId}")
-	public TechNews addTechNews(@RequestBody TechNews techNews, @PathVariable int sessionId) {
-		//TODO: process POST request
-		return techNewsService.addTechNews(techNews, sessionId);
+	public ResponseEntity<?> add(@RequestParam("thumbnail") MultipartFile thumbnail,
+			@RequestParam("name") String name,
+			@RequestParam("description") String description,
+			@PathVariable int sessionId) throws IOException {
+		try {
+			byte[] fileBytes = thumbnail.getBytes();
+			TechNews techNews = new TechNews();
+			techNews.setName(name);
+			techNews.setDescription(description);
+
+			TechNews savedEntity = techNewsService.addTechNews(techNews, sessionId,
+					fileBytes);
+			return ResponseEntity.ok(savedEntity);
+		} catch (IllegalArgumentException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
 	}
-	
+
 	@PutMapping("/{id}")
-	public TechNews updatetTechNews(@PathVariable int id, @RequestBody TechNews techNews) {
-		//TODO: process PUT request
-		return techNewsService.updateTechNews(id, techNews);
+	public ResponseEntity<?> update(@RequestParam(value = "thumbnail", required = false) MultipartFile thumbnail,
+			@RequestParam(value = "name", required = false) String name,
+			@RequestParam(value = "description", required = false) String description,
+			@PathVariable int id) throws IOException {
+		try {
+			byte[] fileBytes = null;
+			if (thumbnail != null) {
+				fileBytes = thumbnail.getBytes();
+			}
+			TechNews techNews = new TechNews();
+			techNews.setDescription(description);
+			techNews.setName(name);
+			TechNews updatedEntity = techNewsService.updateTechNews(id, techNews, fileBytes);
+
+			return ResponseEntity.ok(updatedEntity);
+		} catch (IllegalArgumentException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
 	}
 	
 	@DeleteMapping("/{id}")
--- src/main/java/com/example/livi/service/AboutLiviLifeService.java
+++ src/main/java/com/example/livi/service/AboutLiviLifeService.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.example.livi.model.AboutAwardRecognition;
 import com.example.livi.model.AboutLiviLife;
 import com.example.livi.model.Session;
 import com.example.livi.repository.AboutLiviLifeRepository;
